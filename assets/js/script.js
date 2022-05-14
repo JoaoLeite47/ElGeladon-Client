a2e3829aeb2cc28907c0e5d207be7b6c8be89403
JoaoLeite47 <StalkerJJ@outlook.com>
2022-05-14 03:09:43 -0300

    feat(edit and create models)

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ async function findAllPaletas() {
     document.querySelector('#paletaList').insertAdjacentHTML(
       'beforeend',
       `
-    <div class="PaletaListaItem">
+    <div class="PaletaListaItem" id="PaletaListaItem_${paleta.id}">
         <div>
             <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
             <div class="PaletaListaItem__preco">R$: ${paleta.preco}</div>
@@ -19,7 +19,7 @@ async function findAllPaletas() {
 
             
              <div class="PaletaListaItem_acoes acoes">
-                <button class="acoes-editar btn">Editar</button>
+                <button class="acoes-editar btn" onclick="abrirModal(${paleta.id})">Editar</button>
                 <button class="acoes-apagar btn" >Apagar</button>
               </div>
 
@@ -43,49 +43,83 @@ async function findByIdPaletas() {
   const paletaEscolhidaDiv = document.querySelector('#paletaEscolhida'); //pega o elemento e intera como html
 
   paletaEscolhidaDiv.innerHTML = ` 
-    <div class="PaletaCardItem">
+    <div class="PaletaCardItem" id="PaletaListaItem_${paleta.id}">
     <div>
         <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
         <div class="PaletaCardItem__preco">R$: ${paleta.preco}</div>
         <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+
+        <div class="PaletaListaItem_acoes acoes">
+        <button class="acoes-editar btn" onclick="abrirModal(${paleta.id})">Editar</button>
+        <button class="acoes-apagar btn" >Apagar</button>
+      </div>
+
     </div>
       <img class="PaletaCardItem__foto" src="${paleta.foto}" alt="${paleta.sabor}" />
 </div>
 `;
 }
 
-findAllPaletas();
+findAllPaletas(); //chama a função findAllPaletas
 
-function abrirModalCadastro() {
+async function abrirModal(id = null) {
   //abrir modal
+
+  if (id != null) {
+    //se o id for diferente de null
+    document.querySelector('#title-header-modal').innerText =
+      'Atualizar Paleta'; //altera o titulo do modal
+
+    document.querySelector('#button-form-modal').innerText = 'Atualizar'; //altera o botão do modal
+
+    const response = await fetch(`${baseURL}/paleta/${id}`); //fetch é uma função nativa do js com base na roda get by id no backend
+    const paleta = await response.json(); //retorna um json
+
+    document.querySelector('#sabor').value = paleta.sabor; //altera o valor do input para o valor do sabor da paleta
+    document.querySelector('#preco').value = paleta.preco; //altera o valor do input para o valor do preco da paleta
+    document.querySelector('#descricao').value = paleta.descricao; //altera o valor do input para o valor do descricao da paleta
+    document.querySelector('#foto').value = paleta.foto; //altera o valor do input para o valor do foto da paleta
+    document.querySelector('#id').value = paleta.id;
+  } else {
+    document.querySelector('#title-header-modal').innerText =
+      'Cadastrar Paleta'; //altera o titulo do modal de volta
+    document.querySelector('#button-form-modal').innerText = 'Cadastrar'; //altera o botão do modal de volta
+  }
+
   document.querySelector('.modal-overlay').style.display = 'flex'; //mostra o modal
 }
 
-function fecharModalCadastro() {
+function fecharModal() {
   //fechar modal
   document.querySelector('.modal-overlay').style.display = 'none'; //esconde o modal
-  document.querySelector("#sabor").value = "";
-  document.querySelector("#preco").value = 0;
-  document.querySelector("#descricao").value = "";
-  document.querySelector("#foto").value = "";
+  document.querySelector('#sabor').value = '';
+  document.querySelector('#preco').value = 0;
+  document.querySelector('#descricao').value = '';
+  document.querySelector('#foto').value = '';
 }
 
 async function createPaleta(event) {
   //criar paleta
-  const sabor = document.querySelector('#sabor').value;
-  const preco = document.querySelector('#preco').value;
-  const descricao = document.querySelector('#descricao').value;
-  const foto = document.querySelector('#foto').value;
-
+  const id = document.getElementById("id").value;
+  const sabor = document.getElementById("sabor").value;
+  const descricao = document.getElementById("descricao").value;
+  const foto = document.getElementById("foto").value;
+  const preco = +document.getElementById("preco").value;
   const paleta = {
+    id,
     sabor,
     preco,
     descricao,
     foto,
   };
-  const response = await fetch(`${baseURL}/create`, {
+  const modoEdicaoAtivado = id > 0; //se o id for maior que 0 então é modo edição
+
+  const endpoint =
+    baseURL + (modoEdicaoAtivado ? `/update/${id}` : `/create/${id}`); //se for modo edição, altera o endpoint para update, se não, altera para create
+
+  const response = await fetch(endpoint, {
     //fetch é uma função nativa do js com base na roda post no backend
-    method: 'post',
+    method: modoEdicaoAtivado ? 'put' : 'post', //se for modo edição, altera o metodo para put, se não, altera para post
     headers: {
       'Content-Type': 'application/json', //tipo de conteudo enviado como json
     },
@@ -95,21 +129,28 @@ async function createPaleta(event) {
 
   const novaPaleta = await response.json(); //retorna um json
 
-  const html = `<div class="PaletaListaItem">
+  const html = `<div class="PaletaListaItem" id="PaletaListaItem_${paleta.id}">
   <div>
     <div class="PaletaListaItem__sabor">${novaPaleta.sabor}</div>
     <div class="PaletaListaItem__preco">R$ ${novaPaleta.preco}</div>
     <div class="PaletaListaItem__descricao">${novaPaleta.descricao}</div>
+
+    <div class="PaletaListaItem_acoes acoes">
+    <button class="acoes-editar btn" onclick="abrirModal(${paleta.id})">Editar</button>
+    <button class="acoes-apagar btn" >Apagar</button>
+  </div>
+
   </div>
     <img class="PaletaListaItem__foto" src=${
       novaPaleta.foto
     } alt=${`Paleta de ${novaPaleta.sabor}`} />
   </div>`;
 
-  document.querySelector('#paletaList').insertAdjacentHTML('beforeend', html); //insere o html no html
+  if (modoEdicaoAtivado) {
+    document.querySelector(`#PaletaListaItem_${id}`).outerHTML = html; //altera o html do elemento pelo html da nova paleta
+  } else {
+    document.querySelector('#paletaList').insertAdjacentHTML('beforeend', html); //insere o html no html
+  }
 
-
-  fecharModalCadastro(); //fecha o modal 
+  fecharModal(); //fecha o modal
 }
-
-
